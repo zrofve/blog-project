115806cbc59960e5e17c230bc5c1fd5e79ad18c7
zrofve <zrofvedev@gmail.com>
2025-09-27 23:26:15 +0800

    Add path to those CRUD views in blog/urls.py

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('post/<int:pk>', views.post_details, name='post-detail'),
+    path('post/new', views.PostCreateView.as_view(), name='post-create'),
+    path('post/<int:pk>/edit', views.PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete', views.PostUDeleteView.as_view(), name='post-delete'),
 ]
