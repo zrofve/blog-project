228a8416c0aa06dbb4bd2f94de6b982c53e1a353
zrofve <zrofvedev@gmail.com>
2025-09-27 23:14:28 +0800

    Add class base views in Post app

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Post
+from django.views.generic import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
-# Create your views here.
+# Create your function base views here.
+
+# Home
 def home(request):
     posts = Post.objects.all()
     return render(request, 'blog/home.html', {'posts':posts})
+
+# View Post Details
+def post_details(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+# Create your class base views here.
+
+# Create New Post
+class PostCreateView(CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('home')
+
+# Update Post
+class PostUpdateView(UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content']
+    success_url = reverse_lazy('home')
+
+# Delete Post
+class PostDeleteView(DeleteView):
+    model = Post
+    template_name = 'blog/post_delete.html'
+    success_url = reverse_lazy('home')
