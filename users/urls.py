4d141eaca8d2d8699fa78af546c203a6b76d3bce
zrofve <zrofvedev@gmail.com>
2025-09-28 14:37:52 +0800

    Update users/urls.py

--- users/urls.py
+++ users/urls.py
@@ -9,6 +9,10 @@ def redirect_to_login(request):
     return redirect('login')
 
 urlpatterns = [
-    path('/admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('register/', user_views.register, name='register'),
+    path('profile/', user_views.profile, name='profile'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html', next_page='login'), name='logout'),
+    path('blog/', include('blogs.urls')),
 ]
