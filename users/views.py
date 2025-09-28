c317306887f50529204b7f2b2ced36bef0be82d9
zrofve <zrofvedev@gmail.com>
2025-09-28 14:15:09 +0800

    update users/views.py

--- users/views.py
+++ users/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import UserRegisterForm #this is a class from our forms.py
 
 # Create your views here.
+
+def register(request):
+    if request.method == "POST"
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data('username')
+            messages.success(request, f"Account was created for: {username}!")
