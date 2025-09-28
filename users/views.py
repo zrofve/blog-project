d807a3c59554441ad1a2259a4a54a537153ee1a5
zrofve <zrofvedev@gmail.com>
2025-09-28 18:27:00 +0800

    Fix on users/urls.py

--- users/views.py
+++ users/views.py
@@ -15,7 +15,7 @@ def register(request):
             return redirect('login')
     else:
         form = UserRegisterForm()
-    return render(request, 'users/register.html')
+    return render(request, 'users/register.html', {'form': form})
 
 
 @login_required
