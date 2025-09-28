dbf32f1952504b8061eabb99375039bd7e11c1f9
zrofve <zrofvedev@gmail.com>
2025-09-28 17:42:36 +0800

    Update template - logout.html, need to refactor, follow previous project logout template

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, get_object_or_404
 from .models import Post
 from django.views.generic import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+from django.contrib.auth.decorators import login_required
 
 # Create your function base views here.
 
@@ -11,6 +12,7 @@ def home(request):
     return render(request, 'blog/home.html', {'posts':posts})
 
 # View Post Details
+@login_required
 def post_details(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
